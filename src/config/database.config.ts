feb1bd760e0df894f51affe13a0d4b67a3b77c82
Ijaj29 <123533199+Ijaj29@users.noreply.github.com>
2026-06-16 09:29:22 +0000

    update

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -8,7 +8,7 @@ const config: TypeOrmModuleOptions = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306'),
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD ?? 'password',
   database: process.env.DB_NAME || 'live_streaming',
   entities: [LiveStream, StreamChat, User],
   synchronize: process.env.NODE_ENV === 'production' ? false : true,
